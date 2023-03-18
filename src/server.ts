7dbcc05cc43ab4507892c60e4b9e60e119522b52
Estate Nweke <motivatedestate@gmail.com>
2023-03-18 10:39:16 +0100

    connected to database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import "dotenv/config";
+import mongoose from "mongoose";
+import type { ConnectOptions } from "mongoose"; 
 import app from "./middlewares/appMiddle";
 
 const port = process.env.PORT || 5353;
+
+//Database Connection
+const DB = `${process.env.DATABASE}`;
+const options: ConnectOptions = {
+  retryWrites: true,
+  w: "majority",
+};
+mongoose.set("strictQuery", false);
+mongoose.connect(DB, options)
+  .then(() => {
+    console.log("DB connected successfully!");
+  })
+  .catch((error) => {
+    console.log("Not connected to the database!!", error.stack);
+  });
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
